9b51803aa9cae12087f7cce0f3cc577018c59bf6
Andrew Crites <acrites@mobiquityinc.com>
2013-10-01 13:13:50 -0400

    Dependency management via bower

--- js/require.config.js
+++ js/require.config.js
@@ -1,8 +1,8 @@
 require.config({
     baseUrl: "js",
     paths: {
-        jquery: "//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min",
-        knockout: "//cdnjs.cloudflare.com/ajax/libs/knockout/2.2.1/knockout-min"
+        jquery: "vendor/jquery/jquery.min",
+        knockout: "vendor/knockout.js/knockout"
     },
 });
 require(['urls'], function (app) {
